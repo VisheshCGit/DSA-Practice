259f46d33a8b17f2a277d26ce36be1a97c28daf3
Vishesh Mishra <vmishra114.btech2025@cse.nitrr.ac.in>
2026-06-03 23:05:47 +0530

    Set Matrix Zeroes fron SDE Sheet

---
+++ Arrays/Set_Matrix_Zeroes_LC#73.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void setZeroes(vector<vector<int>>& matrix) {
+int m = matrix.size();
+        int n = matrix[0].size();
+        int col0 = 1;
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0)
+                col0 = 0;
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 1; j--) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+            if(col0 == 0)
+                matrix[i][0] = 0;
+        }
+}
+
+void printMatrix(vector<vector<int>>& matrix) {
+
+    cout << "\nMatrix after setting zeroes:\n";
+    for (int i = 0; i < matrix.size(); i++) {
+       for (int j = 0; j < matrix[0].size(); j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int m, n;
+    cout << "Please enter number of rows: ";
+    cin >> m;
+    cout << "Please enter number of columns: ";
+    cin >> n;
+    vector<vector<int>> matrix(m, vector<int>(n));
+    cout << "Please enter matrix elements:\n";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+    setZeroes(matrix);
+    printMatrix(matrix);
+    return 0;
+}
