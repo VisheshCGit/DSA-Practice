496aaa6ca554f1a90ff9de24cb537fe5d24e7141
Vishesh Mishra <vmishra114.btech2025@cse.nitrr.ac.in>
2026-06-05 17:05:02 +0530

    Permutations Without Recusrion SDE Sheet

---
+++ Recursion And DP/Important Algo's And Questions/Next_Permutation_LC#31.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void nextPermutation(vector<int>& nums) {
+    int index = -1;
+    int n = nums.size();
+
+    for(int i = n - 2; i >= 0; i--) {
+        if(nums[i + 1] > nums[i]) {
+            index = i;
+            break;
+        }
+    }
+
+    if(index == -1) {
+        reverse(nums.begin(), nums.end());
+    }
+    else {
+        for(int i = n - 1; i > index; i--) {
+            if(nums[i] > nums[index]) {
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+
+        reverse(nums.begin() + index + 1, nums.end());
+    }
+}
+
+int main() {
+    int n;
+
+    cout << "Please enter array length: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Please enter array elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    nextPermutation(nums);
+
+    cout << "Next permutation: ";
+    for(int i = 0; i < n; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
