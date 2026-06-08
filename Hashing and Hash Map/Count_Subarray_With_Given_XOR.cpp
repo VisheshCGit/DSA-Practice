9f01387cd373c067198d52a11e44624843cbcdf7
Vishesh Mishra <vmishra114.btech2025@cse.nitrr.ac.in>
2026-06-09 00:10:46 +0530

    SDE Sheet - finding subarrays with given XOR (hashing)

--- Hashing and Hash Map/Count_Subarray_With_Given_XOR.cpp
+++ Hashing and Hash Map/Count_Subarray_With_Given_XOR.cpp
@@ -8,8 +8,8 @@ int subarraysWithXorK(vector<int>& nums, int k) {
     int xr = 0;
     int ans = 0;
 
-    unordered_map<int, int> mp;
-    mp[0] = 1;
+    unordered_map<int, int> mp; //Hashmap will store the frequency.. how many times our required prefix is occurring...
+    mp[0] = 1; //Important
 
     for(int i = 0; i < n; i++) {
         xr ^= nums[i];
