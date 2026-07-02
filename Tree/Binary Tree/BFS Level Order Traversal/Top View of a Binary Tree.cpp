daab908dd6d6c60a8f36d11d6045fdd67fe5af08
Vishesh Mishra <vmishra114.btech2025@cse.nitrr.ac.in>
2026-07-02 09:27:40 +0530

    SDE Sheet Challenge BFS + HD concept

---
+++ Tree/Binary Tree/BFS Level Order Traversal/Top View of a Binary Tree.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+using namespace std;
+
+struct TreeNode{
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int val){
+        data=val;
+        left=nullptr;
+        right=nullptr;
+    }
+};
+
+vector<int> topView(TreeNode *root){
+
+    vector<int> ans;
+
+    if(root==nullptr)
+        return ans;
+
+    queue<pair<TreeNode*, int>> q;
+    map<int, int> mp;
+
+    q.push({root, 0});
+
+    while(!q.empty()){
+
+        auto curr=q.front();
+        q.pop();
+
+        TreeNode *node=curr.first;
+        int hd=curr.second;
+
+        if(mp.find(hd)==mp.end())
+            mp[hd]=node->data;
+
+        if(node->left)
+            q.push({node->left, hd-1});
+
+        if(node->right)
+            q.push({node->right, hd+1});
+    }
+
+    for(auto &x: mp)
+        ans.push_back(x.second);
+
+    return ans;
+}
+
+int main(){
+
+    TreeNode *root=new TreeNode(1);
+    root->left=new TreeNode(2);
+    root->right=new TreeNode(3);
+    root->left->right=new TreeNode(4);
+    root->left->right->right=new TreeNode(5);
+
+    vector<int> ans=topView(root);
+
+    cout<<"Top View: ";
+
+    for(int x: ans)
+        cout<<x<<" ";
+
+    return 0;
+}
