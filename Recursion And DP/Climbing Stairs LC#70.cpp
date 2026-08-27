ee4f9abf206717f0021f5ed284c62f82bdd771a3
Vishesh Mishra <vmishra114.btech2025@cse.nitrr.ac.in>
2026-08-27 23:00:36 +0530

    Optimized using 2 variables or Tabulation method

---
+++ Recursion And DP/Climbing Stairs LC#70.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Solution{
+public:
+    int climbStairs(int n){
+        int prev2=1,prev1=1;
+
+        for(int i=2;i<=n;i++){
+            int curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+
+        return prev1;
+    }
+};
+
+int main(){
+    int n;
+
+    cout<<"Enter n: ";
+    cin>>n;
+
+    Solution obj;
+
+    cout<<"Number of ways: "<<obj.climbStairs(n);
+
+    return 0;
+}
